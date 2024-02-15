cbbb98234e62748a5d24212e9ee738fb58c1a671
Anderson Fernandes <cabula21@yahoo.com.br>
2024-02-15 17:01:38 -0300

    EPM- Ajuste novo

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
 
 export default function App() {
   return (
