364ba7b0e03a9a17821415bde665fe04154c23ff
Anderson Fernandes <cabula21@yahoo.com.br>
2024-03-03 13:18:05 -0300

    EPM - TELA PAINEL

--- screens/Painel.js
+++ screens/Painel.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, ImageBackground, TouchableOpacity, TouchableWithoutFeedback, Keyboard, StyleSheet, Button, Pressable, ScrollView } from 'react-native';
+import { View, Text, ImageBackground, TouchableOpacity, Keyboard, StyleSheet, Button, Pressable, ScrollView, Platform } from 'react-native';
 import { SafeAreaView } from "react-native-safe-area-context";
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { useNavigation } from '@react-navigation/native';
@@ -10,46 +10,49 @@ const Painel = () => {
     const [date, setDate] = useState(new Date());
     const [mode, setMode] = useState('date');
     const [show, setShow] = useState(false);
-    const [entryTime, setEntryTime] = useState();
+    const [entryTime, setEntryTime] = useState(null);
     const [exitTime, setExitTime] = useState(null);
+    const [dateEntrada, setDateEntrada] = useState(new Date());
+    const [dateSaida, setDateSaida] = useState(new Date());
+    const [showEntrada, setShowEntrada] = useState(false);
+    const [showSaida, setShowSaida] = useState(false);
 
-    const onChange = (event, selectedDate) => {
-        const currentDate = selectedDate || date;
+    const onChange = (event, selectedDateTime) => {
+        const currentDate = selectedDateTime || date;
         setShow(false);
-        setDate(currentDate);
-        if (mode === 'time') {
-            if (!entryTime) {
-                setEntryTime(currentDate);
-            } else {
-                setExitTime(currentDate);
-            }
+        
+        if (mode === 'date' && !dateEntrada) {
+            setDateEntrada(currentDate);
+            setMode('time');
+        } else if (mode === 'time' && !entryTime) {
+            setEntryTime(currentDate);
+            setMode('date');
+        } else if (mode === 'date' && dateEntrada && entryTime && !dateSaida) {
+            setDateSaida(currentDate);
+            setMode('time');
+        } else if (mode === 'time' && dateEntrada && entryTime && dateSaida && !exitTime) {
+            setExitTime(currentDate);
+            setMode('date');
+        } else {
+            setDateEntrada(null);
+            setEntryTime(null);
+            setDateSaida(null);
+            setExitTime(null);
+            setMode('date');
         }
-    };    
-
+    };
+    
     const showMode = (currentMode) => {
         setShow(true);
         setMode(currentMode);
     };
 
-    const showDatepicker = () => {
-        showMode('date');        
-    };
-
-    const showTimepicker = () => {
-        showMode('time');
-    };
-
-    const confirmDate = () => {        
-        console.log('Data selecionada:', date);
-        
-        
-    };
-    
     const formatDate = (date) => {
         const options = { day: '2-digit', month: '2-digit' };
-        return date.toLocaleDateString('pt-BR', options).split('/').reverse().join('/');
-      };
-
+        const formatter = new Intl.DateTimeFormat('pt-BR', options);
+        const formattedDate = formatter.format(date);
+        return formattedDate;
+    };
 
     const handleLogout = () => {
         navigation.navigate('Login');
@@ -65,7 +68,7 @@ const Painel = () => {
         >
             
             <SafeAreaView style={{ flex: 1 }}>
-                <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+                <TouchableOpacity onPress={Keyboard.dismiss} accessible={false}>
                     <View style={styles.menuBar}>
                         <TouchableOpacity
                             style={styles.button}
@@ -102,45 +105,55 @@ const Painel = () => {
                             <Text style={styles.buttonText}>Tíckets</Text>
                         </TouchableOpacity>
                     </View>
-                </TouchableWithoutFeedback>
+                </TouchableOpacity>
 
                 <ScrollView>
 
                 <View style={{ flexDirection: 'row', marginTop: 5, backgroundColor: '#fff', borderRadius: 0, shadowColor: '#000', shadowOffset: { width: 0, height: 2 }, shadowOpacity: 0.25, shadowRadius: 3.84, elevation: 5 }}>
-                    <View style={{ flex: 1, padding: 10 }}>
-                        <Text>Placa: ABC-1234</Text>
-                        <View style={{ marginBottom: 5 }}></View>
-                        <Text>Veículo: Carro</Text>
-                        <View style={{ marginBottom: 5 }}></View>                        
-                        <Text>Data: {date.toLocaleDateString('pt-BR', { day: '2-digit', month: '2-digit' })}</Text>
-                        <View style={{ marginBottom: 5 }}></View>
-                        <Text>Hora de entrada: {entryTime ? entryTime.getHours().toString().padStart(2, '0') + ':' + entryTime.getMinutes().toString().padStart(2, '0') : '-'}</Text>
-                        <View style={{ marginBottom: 5 }}></View>
-                        <Text>Hora de saída: {exitTime ? exitTime.getHours().toString().padStart(2, '0') + ':' + exitTime.getMinutes().toString().padStart(2, '0') : '-'}</Text>
-                        <Text>Regra: Até 2h + Taxa</Text>
-                        <View style={{ marginBottom: 5 }}></View>
-                        <Text>Total: R$10,00</Text>
-                    </View>
+                    <View style={{ flex: 1, padding: 15 }}>
+                    <Text>Placa: ABC-1234</Text>
+                    <View style={{ marginBottom: 10 }}></View>
+
+                    <Text>Veículo: Carro</Text>
+                    <View style={{ marginBottom: 10 }}></View>
+
+                    <Text>Data Entrada: {formatDate(dateEntrada)}</Text>
+                    <View style={{ marginBottom: 10 }}></View>
+
+                    <Text>Hora de entrada: {entryTime ? entryTime.getHours().toString().padStart(2, '0') + ':' + entryTime.getMinutes().toString().padStart(2, '0') : '-'}</Text>
+                    <View style={{ marginBottom: 10 }}></View>
+
+                    <Text>Data Saída: {formatDate(dateSaida)}</Text>
+                    <View style={{ marginBottom: 10 }}></View>
+
+                    <Text>Hora de saída: {exitTime ? exitTime.getHours().toString().padStart(2, '0') + ':' + exitTime.getMinutes().toString().padStart(2, '0') : '-'}</Text>
+                    <View style={{ marginBottom: 10 }}></View>
+
+                    <Text>Regra: (Até 2h + Taxa)</Text>
+                    <View style={{ marginBottom: 10 }}></View>
+
+                    <Text>Total: R$10,00</Text>
+                    </View>                 
 
 
+                <View style={{ width: 1, backgroundColor: 'grey', marginRight: 70 }} />
 
-                    <View style={{ width: 1, backgroundColor: 'grey', marginRight: 10 }} />
 
+                <View style={{ flexDirection: 'column', alignItems: 'center', marginRight: 60 }}>
 
-                    <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                        <View style={{ alignItems: 'center', marginRight: 20 }}>
-                            <Text style={{ fontSize: 40, color: "red", fontWeight: "bold" }}>5</Text>
-                            <Text style={{ fontSize: 15 }}>OCUPADAS</Text>
+                        <View style={{ alignItems: 'center', marginBottom: 20 }}>
+                            <Text style={{ fontSize: 35, color: "red", fontWeight: "bold" }}>5</Text>
+                            <Text style={{ fontSize: 12 }}>OCUPADAS</Text>
                         </View>
 
-                        <View style={{ alignItems: 'center', marginRight: 20 }}>
-                            <Text style={{ fontSize: 40, color: "green", fontWeight: "bold" }}>15</Text>
-                            <Text style={{ fontSize: 15 }}>LIVRE</Text>
+                        <View style={{ alignItems: 'center', marginBottom: 20 }}>
+                            <Text style={{ fontSize: 35, color: "green", fontWeight: "bold" }}>15</Text>
+                            <Text style={{ fontSize: 12 }}>LIVRE</Text>
                         </View>
 
-                        <View style={{ alignItems: 'center', marginRight: 20 }}>
-                            <Text style={{ fontSize: 40, color: "#191970", fontWeight: "bold" }}>20</Text>
-                            <Text style={{ fontSize: 15 }}>TOTAL</Text>
+                        <View style={{ alignItems: 'center', marginBottom: 20 }}>
+                            <Text style={{ fontSize: 35, color: "#191970", fontWeight: "bold" }}>20</Text>
+                            <Text style={{ fontSize: 12 }}>TOTAL</Text>
                         </View>
                     </View>
                 </View>
@@ -153,37 +166,38 @@ const Painel = () => {
                         <Text style={{ color: 'blue', fontWeight: 'bold' }}>Acesse</Text>
                     </Pressable>
                 </View>
-                <View style={{ marginTop: 20, width: 250, marginLeft: 70, borderRadius: 50, }}>
-                    <Button onPress={showDatepicker} title="Dia Entrada" />
-                </View>
-                <View style={{ marginTop: 20, width: 250, marginLeft: 70, borderRadius: 50 }}>
-                    <Button onPress={showTimepicker} title="Horário de Entrada" />
-                </View>
-                <View style={{ marginTop: 20, width: 250, marginLeft: 70, borderRadius: 50, }}>
-                    <Button onPress={showDatepicker} title="Dia Saída" />
-                </View>
 
-                <View style={{ marginTop: 20, width: 250, marginLeft: 70, borderRadius: 50 }}>
-                    <Button onPress={showTimepicker} title="Horário de Saída" />
-                </View>
-
-                {/* <Text style={{ marginTop: 20, backgroundColor: "#191970", padding: 20, color: "white", width: 250, marginLeft: 70, borderRadius: 5 }}>
-                    Data selecionada: {date.toLocaleDateString('pt-BR', { day: '2-digit', month: '2-digit' })}{"\n"}
-                    Horário de entrada: {entryTime ? entryTime.getHours().toString().padStart(2, '0') + ':' + entryTime.getMinutes().toString().padStart(2, '0') : '-'}{"\n"}
-                    Horário de saída: {exitTime ? exitTime.getHours().toString().padStart(2, '0') + ':' + exitTime.getMinutes().toString().padStart(2, '0') : '-'}
-                </Text> */}
+                <View style={{ marginVertical: 15, marginTop: 20 }}>
+                    <View style={{ marginHorizontal: 50 }}>
+                        <Button onPress={() => showMode('date')} title="Data Entrada" />
+                        <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center', marginTop: 10 }}>
+                        <Icon name="chevron-down" size={20} color="black" />
+                        </View>
+                        <Button onPress={() => showMode('time')} title="Hora Entrada" />
+                        <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center', marginTop: 10 }}>
+                        <Icon name="chevron-down" size={20} color="black" />
+                        </View>
+                        <Button onPress={() => showMode('date')} title="Data Saída" />
+                        <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center', marginTop: 10 }}>
+                        <Icon name="chevron-down" size={20} color="black" />
+                        </View>
+                        <Button onPress={() => showMode('time')} title="Hora Saída" />
+                    </View>
+                    </View>
+                
                 {show && (
-                    <DateTimePicker
+                <DateTimePicker
                     testID="dateTimePicker"
                     value={date}
                     mode={mode}
                     is24Hour={true}
-                    display="spinner"
+                    display="default"
                     onChange={onChange}                    
-                    />
-                )}
+                />
+            )}
+               
 
-                <TouchableOpacity onPress={() => navigation.navigate("Tickets")} style={{ marginTop: 20, width: 200, marginLeft: 90, alignItems: 'center', borderRadius: 10, backgroundColor: 'green' }}>
+                <TouchableOpacity onPress={() => navigation.navigate("Tickets")} style={{ marginTop: 5, width: 200, marginLeft: 90, alignItems: 'center', borderRadius: 5, backgroundColor: 'green' }}>
                     <View style={{ flexDirection: 'row', alignItems: 'center' }}>
                         <Icon name="ticket" size={40} color="white" />
                         <Text style={{ marginLeft: 10, color: 'white' }}>Gerar Ticket</Text>
@@ -196,6 +210,7 @@ const Painel = () => {
                     <Icon name="sign-out" size={24} color="black" />
                     <Text style={styles.logoutText}>Sair</Text>
                 </TouchableOpacity>
+                
             </SafeAreaView>
         </ImageBackground>
     );
